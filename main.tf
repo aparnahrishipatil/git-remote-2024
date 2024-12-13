7b4e708e0939f86c081a0cbb29d70a6c4c2bd8ab
Testuser <Testuser@exam.com>
2024-12-13 17:34:25 +0530

    modifying property

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 // https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
 resource "aws_vpc" "terraform_test_vpc" {
   cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
+  enable_dns_hostnames = false
   enable_dns_support   = false
 
   tags = {
